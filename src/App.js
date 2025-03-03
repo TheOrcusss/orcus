2d6976a2a6e8ab6d7900ee39b7092c7e9b681063
Orcus <yashbpatel.2745@gmail.com>
2025-03-03 22:18:25 +0530

    exp4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import DisplayText from "./Components/DisplayText"; 
+import ClassDisplayText from "./Components/ClassDisplayText";
+import Counter from "./Components/Counter"; 
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; 
+import Home from "./Components/Home";
+import About from "./Components/About"; 
+import Contact from "./Components/Contact";
 
-function App() {
+
+function App() 
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Functional Component Example</h1>
+      <DisplayText/>
+
+      <hr></hr>
+
+      <h1>React Class Component Example</h1>
+      <ClassDisplayText/>
+
+      <hr></hr>
+
+      <h1>React State Example</h1>
+      <Counter /> 
+      
+      <hr></hr>
+      
+      <Router>
+        <div>
+          <h1>React Single Page Application</h1>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+            </ul>
+          </nav>
+
+          {/* Define Routes */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </Router>
+
     </div>
   );
 }
---
+++ src/Components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => 
+{ 
+    return (
+        <div>
+            <h2>About Page</h2>
+            <p>This is a simple SPA built using React Router.</p>
+        </div>
+    );
+};
+
+export default About;
+    
